5a4a7fc0b51ae115e5e88a5eaad7545e71720283
jdominguez <jdominguezm1300@alumno.ipn.mx>
2021-06-13 15:16:28 -0500

    Create conventions.py

---
+++ conventions.py
@@ -0,0 +1,11 @@
+"""
+Author: Joaquín Domínguez Moran
+Start Date: 13.06.2021
+Last Update: 13.06.2021
+"""
+#In this part I indicate the name conventios for these modules 
+import numpy as pd
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import statsmodels as sm 
